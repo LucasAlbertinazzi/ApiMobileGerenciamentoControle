2496fc7f94d8f5fd2ae5337f29cb82905c03d6a7
lucas <lucasgalbertinazzi98@gmail.com>
2024-02-19 16:03:12 -0300

    v6

--- Models/TblOcorrUsuario.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace API_GerenciamentoGerenciamentoControle_Controle.Models;
-
-public partial class TblOcorrUsuario
-{
-    public short Codusuario { get; set; }
-}
